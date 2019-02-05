ec48cc4a9b1ebd09d4a7fde3612c482f76a48ec5
Arattan <amanrattan21@gmail.com>
2019-02-05 19:34:07 +0100

    Added solution code: new BodyPartFragment class and instantiation of headFragment

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,8 +16,9 @@
 
 package com.example.android.android_me.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.android_me.R;
 
@@ -32,13 +33,17 @@ public class AndroidMeActivity extends AppCompatActivity {
         // TODO (3) Show the first image in the list of head images
             // Soon, you'll update this image display code to show any image you want
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
         // TODO (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        BodyPartFragment headFragment = new BodyPartFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().add(R.id.head_container, headFragment)
+                .commit();
     }
 }
